2692b52bd32de22d8b290b58b191f8e6ce1f19ac
yoshii <yoshiikempo@gmail.com>
2024-08-27 09:00:39 +0900

    プルダウンメニューの改修#2

--- post.php
+++ post.php
@@ -1,6 +1,5 @@
 <?php
 
-// use Illuminate\Support\Facades\DB;
 date_default_timezone_set('Asia/Tokyo');
 
 include "./dba/pgConnection.php";
@@ -8,7 +7,7 @@ include "./dba/pgConnection.php";
 $dbh = null;
 try {
     
-    // データベース接続する
+    // データベースに接続する
     global $dbh;
     $dbh = connectToDb();
     $dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
@@ -16,77 +15,111 @@ try {
     // 
     $userId = 1;
     $text = htmlspecialchars($_POST["text"]);
-    $majorCategory = htmlspecialchars($_POST["category1"]);
-    $minorCategory = htmlspecialchars($_POST["category2"]);
+    $majorCategory = htmlspecialchars($_POST["mainCategory"]);
+    $minorCategory = htmlspecialchars($_POST["subCategory"]);
     $createdAt = date("Y-m-d H:i:s");
+    $inputMiss;
 
-    // チェックリストの値を調べる
-    switch ($majorCategory) {
-        case "frontend":
-            $majorCategory = 1;
-            break;
-        case "backend":
-            $majorCategory = 2;
-            break;
-        case "infrastructure":
-            $majorCategory = 3;
-            break;
-        case "game":
-            $majorCategory = 4;
-            break;
-        case "code":
-            $majorCategory = 5;
-            break;
-    }
-    switch ($minorCategory) {
-        case "html":
-            $minorCategory = 1;
-            break;
-        case "css":
-            $minorCategory = 2;
-            break;
-        case "js":
-            $minorCategory = 3;
-            break;
-        case "py":
-            $minorCategory = 4;
-            break;
-        case "java":
-            $minorCategory = 5;
-            break;
-        case "ruby":
-            $minorCategory = 6;
-            break;
-        case "php":
-            $minorCategory = 7;
-            break;
-        case "cs":
-            $minorCategory = 8;
-            break;
-        case "cpp":
-            $minorCategory = 9;
-            break;
-        case "swift":
-            $minorCategory = 10;
-            break;
+    // カテゴリの未入力チェック
+    if ($majorCategory == null || $majorCategory == "") {
+        disconnectDb();
+        global $inputMiss;
+        $inputMiss = null;
+        echo "<h1>カテゴリーを選択してください</h1>";
+        var_dump($inputMiss);
+        require "./index.php";
+        return false;
+    // サブカテゴリの未入力チェック
+    } else if ($majorCategory == null || $majorCategory == "") {
+        disconnectDb();
+        global $inputMiss;
+        $inputMiss = null;
+        echo "<h1>サブカテゴリーを選択してください</h1>";
+        var_dump($inputMiss);
+        require "./index.php";
+        return false;
     }
 
-    $stmt = $dbh->prepare(
-        "insert into articles
-            (user_id, content, created_at, minor_category_id, major_category_id)
-        values
-            (:user_id, :content, :created_at, :minor_category_id, :major_category_id)"
-    );
-        
+    global $inputMiss;
+    if ($inputMiss == null) {
+        $majorCategory = $inputMiss;
+        $minorCategory = $inputMiss;
+        $text = $inputMiss;
+        $createdAt = $inputMiss;
+        var_dump($majorCategory);
+        var_dump($minorCategory);
+        print_r($text);
+        var_dump($createdAt);
+        return;
+    }
 
-    // 入力値の検証(不正値チェック)
-    $stmt->bindValue(':user_id', $userId, PDO::PARAM_INT);
-    $stmt->bindValue(':content', $text, PDO::PARAM_STR);
-    $stmt->bindValue(':created_at', $createdAt, PDO::PARAM_STR);
-    $stmt->bindValue(':minor_category_id', $minorCategory, PDO::PARAM_INT);
-    $stmt->bindValue(':major_category_id', $majorCategory, PDO::PARAM_INT);
+        // カテゴリの内容をセットする
+        switch ($majorCategory) {
+            case "frontend":
+                $majorCategory = 1;
+                break;
+            case "backend":
+                $majorCategory = 2;
+                break;
+            case "infrastructure":
+                $majorCategory = 3;
+                break;
+            case "game":
+                $majorCategory = 4;
+                break;
+            case "code":
+                $majorCategory = 5;
+                break;
+        }
+        switch ($minorCategory) {
+            case "HTML":
+                $minorCategory = 1;
+                break;
+            case "CSS":
+                $minorCategory = 2;
+                break;
+            case "JavaScript":
+                $minorCategory = 3;
+                break;
+            case "Python":
+                $minorCategory = 4;
+                break;
+            case "Java":
+                $minorCategory = 5;
+                break;
+            case "Ruby":
+                $minorCategory = 6;
+                break;
+            case "PHP":
+                $minorCategory = 7;
+                break;
+            case "C#":
+                $minorCategory = 8;
+                break;
+            case "C++":
+                $minorCategory = 9;
+                break;
+            case "Swift":
+                $minorCategory = 10;
+                break;
+        }
 
-    $stmt->execute();
+
+        // $stmt = $dbh->prepare(
+        //     "insert into articles
+        //         (user_id, content, created_at, minor_category_id, major_category_id)
+        //     values
+        //         (:user_id, :content, :created_at, :minor_category_id, :major_category_id)"
+        // );
+
+        // // 入力値の検証(不正値チェック)
+        // $stmt->bindValue(':user_id', $userId, PDO::PARAM_INT);
+        // $stmt->bindValue(':content', $text, PDO::PARAM_STR);
+        // $stmt->bindValue(':created_at', $createdAt, PDO::PARAM_STR);
+        // $stmt->bindValue(':minor_category_id', $minorCategory, PDO::PARAM_INT);
+        // $stmt->bindValue(':major_category_id', $majorCategory, PDO::PARAM_INT);
+        // $stmt->execute();
+
 
 
     global $dbh;
