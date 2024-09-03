3427199874bfa0d77fb5dc13a176ecfb8509d94c
Ogi0021 <112679982+Ogi0021@users.noreply.github.com>
2024-09-03 11:04:11 +0900

    作成開始

---
+++ template/common/検索結果ページモック.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>検索結果</title>
+</head>
+<body>
+    <?php include "header.html" ?>
+
+    <!--この辺に検索結果-->
+    <?php include "footer.html" ?>
+</body>
+</html>
