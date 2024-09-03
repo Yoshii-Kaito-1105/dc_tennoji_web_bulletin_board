7c3421a5f23baeaf1a11fb23428058d25e89fe09
Yoshii-Kaito-1105 <it-tennoji130@dc-itex.com>
2024-09-03 10:51:34 +0900

    効率的なコードの書き方の記事一覧画面の実装

--- common/header.php
+++ common/header.php
@@ -64,7 +64,17 @@
                                 <li href="">C++</li>
                                 <li href="">Swift</li>
                             </ul>
-                        </details>
+                    </details>
+                    <a href="/dc_tennoji_web_bulletin_board/template/contents/code/codeTechnic.php"><li>効率的なコードの書き方</li></a>
+                    <details class="categories-link">
+                        <summary>各項目</summary>                        
+                            <ul id="categories">
+                                各言語
+                                <li href="">C#</li>
+                                <li href="">C++</li>
+                                <li href="">Swift</li>
+                            </ul>
+                    </details>
                 </ul>
 
             </details>
