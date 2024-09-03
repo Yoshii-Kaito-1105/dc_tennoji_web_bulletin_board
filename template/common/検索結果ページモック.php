9610b05159b3ae316d4c169de320e134fa0b74d4
TatsuyaWakebe <103712317+TatsuyaWakebe@users.noreply.github.com>
2024-09-03 11:47:39 +0900

    Update 検索結果ページモック.php

--- template/common/検索結果ページモック.php
+++ template/common/検索結果ページモック.php
@@ -8,7 +8,86 @@
 <body>
     <?php include "header.html" ?>
 
+    <!--検索バー-->
+    <div class="container">
+        <form method="get" action="#" class="search_container">
+        <div class="hissu" >必須</div>カテゴリー：<select name="mainCategory" id="mainCategory">
+                        <option value="">選択してください</option>
+                        <option value="frontend">フロントエンド</option>
+                        <option value="backend">バックエンド</option>
+                        <option value="infrastructure">インフラ</option>
+                        <option value="game">ゲーム</option>
+                        
+                    </select>
+                    
+                    <div class="hissu" >必須</div>サブカテゴリー：<select name="subCategory" id="subCategory">
+                        <option value=" ">選択してください</option>
+                        
+                        
+                    </select>
+                    <input type="text" size="25" placeholder=" キーワード検索">
+                    <input type="submit" value="検索">
+        </form>
+
+    </div>
+
     <!--この辺に検索結果-->
     <?php include "footer.html" ?>
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script>
+        
+
+        // #subCategoryの中身を準備
+        var items = {
+            frontend:[
+                ['選択してください', ''],
+                ['HTML', 'HTML'],
+                ['CSS', 'CSS'],
+                ['JavaScript', 'JavaScript']
+            ],
+            backend:[
+                ['選択してください', ''],
+                ['Python', 'Python'],
+                ['PHP','PHP'],
+                ['Java', 'Java']
+            ],
+            infrastructure:[
+                ['選択してください', ''],
+                ['Python', 'Python'],
+                ['Ruby', 'Ruby'],
+                ['Java', 'Java']
+            ],
+            game:[
+                ['選択してください', ''],
+                ['C++', 'C++'],
+                ['C#', 'C#'],
+                ['Swift', 'Swift']
+            ],
+        };
+
+        var none = $('#subCategory').html(); // #subCategoryの最初の状態
+        $("#subCategory").attr("disabled", true); // #mainCategoryが「選択してください」のときは操作不可に
+
+        $('#mainCategory').on('change', function(){
+            var cont = $(this).val(); // 選択された項目のvalueを取得
+            if(cont){ // valueに何か値が入っていた場合
+                var item = items[cont]; // リストからカテゴリに登録されたサブカテゴリの配列を取得
+                $("#subCategory").attr("disabled", false); // #subCategoryを選択可能に
+                $('#subCategory').html('');
+                var option;
+                for(var i = 0; i < item.length; i++){
+                    option = '<option value="' + item[i][1] + '">' + item[i][0] + '</option>';
+                    $('#subCategory').append(option);
+                }
+            } else { // valueに何も値が入っていない場合
+                $("#subCategory").attr("disabled", true);
+                $('#subCategory').html(none); // 保存された最初の状態に戻す
+            }
+        });
+
+
+
+    </script>
 </body>
 </html>
