1292f5b4e77b41ba9046bccd5bfc2cfc6a1ee731
TatsuyaWakebe <103712317+TatsuyaWakebe@users.noreply.github.com>
2024-09-11 14:27:02 +0900

    関数に変数が引き継がれていない？

--- template/common/表示書き換えテスト.php
+++ template/common/表示書き換えテスト.php
@@ -1,36 +1,4 @@
-<?php
-            
-            // 投稿日と更新日を日本時間に適用する
-            date_default_timezone_set('Asia/Tokyo');
-            // 別階層にあるDBコネクタの読み込み
-            require_once(__DIR__ . "/../../dba/pgConnection.php"); 
-            $dbh = connectToDb();
 
-                        // データベースからレコードを全て取得する
-            $sql = 
-            "select 
-                u.name as user_name,
-                a.created_at,
-                mjrc.name as major_category_name,
-                minc.name as minor_category_name,
-                a.content
-            from 
-                articles a
-            inner join 
-                users u
-            on a.user_id = u.user_id
-            inner join
-                major_categories mjrc
-            on a.major_category_id = mjrc.major_category_id
-            inner join 
-                minor_categories minc
-            on a.minor_category_id = minc.minor_category_id
-            ";
-        $stmt = $dbh->query($sql);
-        $articles = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        // 記事テーブルにあるレコード数を取得する
-        $recodeCount = $stmt->rowCount(); 
-?>
 
 <!DOCTYPE html>
 <html lang="ja">
@@ -45,11 +13,11 @@
         <form method="get" class="search_container">
         カテゴリー：<select name="mainCategory" id="mainCategory">
                         <option value="">選択してください</option>
-                        <option value="frontend">フロントエンド</option>
-                        <option value="backend">バックエンド</option>
-                        <option value="infrastructure">インフラ</option>
-                        <option value="game">ゲーム</option>
-                        <option value="code">効率的なコードの書き方</option>
+                        <option value="1">フロントエンド</option>
+                        <option value="2">バックエンド</option>
+                        <option value="3">インフラ</option>
+                        <option value="4">ゲーム</option>
+                        <option value="5">効率的なコードの書き方</option>
                     </select>
                     
                     サブカテゴリー：<select name="subCategory" id="subCategory">
@@ -63,29 +31,67 @@
     
         <div class="result">
         <?php    
-
-
+            // 投稿日と更新日を日本時間に適用する
+            date_default_timezone_set('Asia/Tokyo');
+            // 別階層にあるDBコネクタの読み込み
+            require_once(__DIR__ . "/../../dba/pgConnection.php"); 
+            $dbh = connectToDb();
+            //データベース接続確認
+            //if ($dbh === null) {
+            //    echo 'データベース接続に失敗しました。';
+            //} else {
+            //    echo 'データベース接続が成功しました。';
+            //}
+            
+                         // データベースからレコードを全て取得する
+            $sql = 
+             "select 
+                 u.name as user_name,
+                 a.created_at,
+                 mjrc.name as major_category_name,
+                 minc.name as minor_category_name,
+                 a.content
+             from 
+                 articles a
+             inner join 
+                 users u
+             on a.user_id = u.user_id
+             inner join
+                 major_categories mjrc
+             on a.major_category_id = mjrc.major_category_id
+             inner join 
+                 minor_categories minc
+             on a.minor_category_id = minc.minor_category_id
+             ";
+            $stmt = $dbh->query($sql);
+            $articles = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
             $text='○○の検索結果：××件';
             $replace=$text;
-            $mainSearchCategory = filter_input(INPUT_POST,"mainCategory") ; // 検索画面のメインカテゴリ
-            $subSearchCategory = filter_input(INPUT_POST,"subCategory"); // 検索画面のサブカテゴリ
-            $keyWord = filter_input(INPUT_POST,"keyWord"); // 検索画面の投稿内容
+            $mainSearchCategory = filter_input(INPUT_POST,"mainCategory",FILTER_VALIDATE_INT) ; // 検索画面のメインカテゴリ
+            
+            $subSearchCategory = filter_input(INPUT_POST,"subCategory",FILTER_VALIDATE_INT); // 検索画面のサブカテゴリ
+
+            $keyWord = filter_input(INPUT_POST,"keyWord",FILTER_SANITIZE_STRING); // 検索画面の投稿内容
             $keyWordWithWildcard = "%" . $keyWord . "%";// 部分一致のためにワイルドカードを追加
-            $searchSwitch=0;//検索フラグ用の関数
+
+            $searchSwitch=0;//検索フラグ用の変数
             function searchBranch($searchSwitch){
-                
+                global $dbh;
+                global $keyWordWithWildcard;
+                //searchSwitch内の数字で分岐
                 switch($searchSwitch){
                 case 0:
                     
                 break;
                 case 1;
                     //キーワード部分一致（以降キーワードはすべて部分一致とする）
-                    //$counts = $dbh->query("SELECT COUNT(article_id) as cnt FROM articles Where content LIKE :keyword");
+                    $counts = $dbh->query("SELECT COUNT(article_id) as cnt FROM articles Where content LIKE :keyword");
+                    //
                 break;
                 case 2:
                     //メイン一致
-                    //$counts = $dbh->query('SELECT COUNT(article_id) as cnt FROM articles');
+                    //$counts = $dbh->query('SELECT COUNT(article_id) as cnt FROM articles where major_category ={$mainSearchCategory}');
                 break;
                 case 3:
                     //メインとキーワード一致
@@ -97,13 +103,25 @@
                 break;
                 case 5:
                     //メイン・サブ・キーワード一致
+                    $query = "SELECT * FROM articles 
+                    WHERE main_category = :mainSearchCategory 
+                    AND sub_category = :subSearchCategory 
+                    AND keyword_column LIKE :keyWord";
                     //$counts = $dbh->query('SELECT COUNT(article_id) as cnt FROM articles');
                 break;
                 
                 }
+                // クエリを実行
+                //$stmt = $pdo->prepare($query);
+                //$stmt->bindParam(':mainSearchCategory', $mainSearchCategory, PDO::PARAM_INT);
+                //$stmt->bindParam(':subSearchCategory', $subSearchCategory, PDO::PARAM_INT);
+                //$stmt->bindParam(':keyWord', $keyWordWithWildcard, PDO::PARAM_STR);
+
+                //$stmt->execute();
                 //$counts = $dbh->query('SELECT COUNT(article_id) as cnt FROM articles');
                 //$count = $counts->fetch();
-                //echo ($count['cnt']);
+                
+                
             }
             if(empty($_POST['search'])){
                 
@@ -198,30 +216,30 @@
         var articles = {
             frontend:[
                 ['選択してください', ''],
-                ['HTML', 'HTML'],
-                ['CSS', 'CSS'],
-                ['JavaScript', 'JavaScript']
+                ['HTML', '1'],
+                ['CSS', '2'],
+                ['JavaScript', '3']
             ],
             backend:[
                 ['選択してください', ''],
-                ['Python', 'Python'],
-                ['PHP','PHP'],
-                ['Java', 'Java']
+                ['Python', '4'],
+                ['PHP','5'],
+                ['Java', '6']
             ],
             infrastructure:[
                 ['選択してください', ''],
-                ['Python', 'Python'],
-                ['Ruby', 'Ruby'],
-                ['Java', 'Java']
+                ['Python', '7'],
+                ['Ruby', '8'],
+                ['Java', '9']
             ],
             game:[
                 ['選択してください', ''],
-                ['C++', 'C++'],
-                ['C#', 'C#'],
-                ['Swift', 'Swift']
+                ['C++', '10'],
+                ['C#', '11'],
+                ['Swift', '12']
             ],
             code:[
-                ['未分類', 'uncategorized']
+                ['未分類', '13']
             ]
         };
 
