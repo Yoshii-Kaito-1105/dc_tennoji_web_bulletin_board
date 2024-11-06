49ad3cf471a89ab1c55c07e6d59a6ed4e30371e3
Yoshii-Kaito-1105 <it-tennoji130@dc-itex.com>
2024-11-06 10:45:49 +0900

    DB接続管理ファイルのコメント

--- dba/pgConnection.php
+++ dba/pgConnection.php
@@ -1,41 +1,46 @@
 <!-- データベース接続管理 -->
 <?php
 
-// データベース
+// ローカルデータベースファイルを読み込む
 include "PgDao.php";
 
+// データベース接続用のグローバル変数
 $dbh = null;
 
+// データベースに接続する関数
 function connectToDb() 
 {
+    // 例外処理(tryで処理を実行)
     try {
-        // クラスをインスタンス化;
-        $dao = new PgDao();
-        $host = $dao->getHost();
-        $port = $dao->getPort();
-        $dbName = $dao->getDbName();
-        $user = $dao->getUserName();
-        $password = $dao->getPassword();
+        
+        $dao = new PgDao(); // クラスをインスタンス化;
+        $host = $dao->getHost(); // ホストを取得
+        $port = $dao->getPort(); // ポート番号を取得
+        $dbName = $dao->getDbName(); // データベース名を取得
+        $user = $dao->getUserName(); //  データベースユーザー名を取得
+        $password = $dao->getPassword(); // データベース接続用パスワードを取得
 
-        global $dbh;
+        global $dbh; // グローバル変数を使用できるようにする
+        /**
+         * PHP付属の関数を使ってPostgreSQLに接続する
+        */
         $dbh = new PDO(
             "pgsql:host=$host;port=$port;dbname=$dbName;user=$user;password=$password"
         );
-        
-        echo "<script>console.log('データベースに接続しました');</script>";
-        return $dbh;
+        echo "<script>console.log('データベースに接続しました');</script>"; // コンソールに接続成功のメッセージを出力する
+        return $dbh; // 接続した情報を返す
 
-    } catch (PDOException $e) {
-        $e->getMessage();
-        echo "<script>console.log('データベース接続に失敗しました');</script>";
+    } catch (PDOException $e) { // 何かしらの例外が発生した際にキャッチする
+        $e->getMessage(); // 例外の情報が記載された情報を画面に出力する
+        echo "<script>console.log('データベース接続に失敗しました');</script>"; // コンソールに接続失敗のメッセージを出力する
     }
     
 }
-// DBの接続を切る
+// DBの接続を切る関数
 function disconnectDb() 
 {
-    echo "<script>console.log('データベースの接続を切断しました');</script>";
-    global $dbh;
-    return $dbh = null;
+    echo "<script>console.log('データベースの接続を切断しました');</script>"; // コンソールに接続切断のメッセージを出力する
+    global $dbh; // グローバル変数を呼び出す
+    return $dbh = null; // nullを代入し、接続を切る
 }
 
